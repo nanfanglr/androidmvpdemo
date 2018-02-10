33883e5b602aa5d989c886b4855a95e167b22413
liurui <1513093228@qq.com>
2018-02-10 14:02:16 +0800

    测试提交

--- app/src/main/java/com/mvp/rui/androidmvpdemo/MainActivity.java
+++ app/src/main/java/com/mvp/rui/androidmvpdemo/MainActivity.java
@@ -3,6 +3,9 @@ package com.mvp.rui.androidmvpdemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 2018.2.10上传GitHub测试
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
