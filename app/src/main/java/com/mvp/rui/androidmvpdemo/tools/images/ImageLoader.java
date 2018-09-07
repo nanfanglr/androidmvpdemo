62821a54dfc7b8d682c7d360b1c4def70ab24816
nanfanglr <nanfanglr@sina.com>
2018-09-07 16:03:25 +0800

    添加修改gradle文件，升级依赖版本

--- app/src/main/java/com/mvp/rui/androidmvpdemo/tools/images/ImageLoader.java
+++ app/src/main/java/com/mvp/rui/androidmvpdemo/tools/images/ImageLoader.java
@@ -1,34 +1,26 @@
 package com.mvp.rui.androidmvpdemo.tools.images;
 
-import android.content.Context;
-import android.net.Uri;
-import android.widget.ImageView;
-
-import com.bumptech.glide.Glide;
-
-import timber.log.Timber;
-
-/**
- * Created by rui on 2018/3/9.
- * Class works as our image loading library wrapper. Allows for easier testing and replacement of the image loading library.
- */
-public class ImageLoader {
-
-    private final Context context;
-
-    public ImageLoader(Context context) {
-
-        this.context = context;
-    }
-
-    public void loadImage(String imageUri, ImageView viewToLoadImageIn) {
-
-        Timber.d("Fetching image from uri: %s", imageUri);
-        Uri uri = Uri.parse(imageUri);
-        Glide.with(context)
-                .load(uri)
-//                .fitCenter()
-                .into(viewToLoadImageIn);
-    }
-
-}
+///**
+// * Created by rui on 2018/3/9.
+// * Class works as our image loading library wrapper. Allows for easier testing and replacement of the image loading library.
+// */
+//public class ImageLoader {
+//
+//    private final Context context;
+//
+//    public ImageLoader(Context context) {
+//
+//        this.context = context;
+//    }
+//
+//    public void loadImage(String imageUri, ImageView viewToLoadImageIn) {
+//
+//        Timber.d("Fetching image from uri: %s", imageUri);
+//        Uri uri = Uri.parse(imageUri);
+//        Glide.with(context)
+//                .load(uri)
+////                .fitCenter()
+//                .into(viewToLoadImageIn);
+//    }
+//
+//}
